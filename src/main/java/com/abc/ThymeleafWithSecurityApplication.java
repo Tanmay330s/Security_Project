baa98f2e8d93270526b68a3ff9ad06a6f4234ab3
Tanmay <tanmayshinde330@gmail.com>
2026-07-02 00:34:41 +0530

    JIRA-102 Completed

--- src/main/java/com/abc/ThymeleafWithSecurityApplication.java
+++ src/main/java/com/abc/ThymeleafWithSecurityApplication.java
@@ -9,6 +9,13 @@ public class ThymeleafWithSecurityApplication {
 	//JIRA-102
 	public void m2(){
 	}
+
+	//JIRA-101
+	public void m1(){
+		int i=10;
+		String name="abc";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafWithSecurityApplication.class, args);
 		int i = 10;
